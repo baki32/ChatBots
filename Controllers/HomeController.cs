92252ebcc0a9ac93d5df90fa424e002219034ed1
Tomas Bakita <tomas.bakita@outlook.com>
2018-04-07 21:55:25 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace SimpleEchoBot.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET api/<controller>
+        public ActionResult Index()
+        {
+            Microsoft.ApplicationInsights.TelemetryClient client = new Microsoft.ApplicationInsights.TelemetryClient();
+            client.InstrumentationKey = "40513e3d-af87-4816-8b49-147dd2e111de";
+            client.TrackTrace("ZIDAN", Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Critical, new Dictionary<string, string> { { "ZI", "DAN" } });
+            return View();
+        }
+
+        
+    }
+}
